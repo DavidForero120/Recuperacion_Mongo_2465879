727527197f6cf59809dc4875b79f59df8758684d
DavidForero120 <DavidForero120>
2022-11-13 13:49:44 -0500

    structure-first-question

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+
+
+const comienzo = document.getElementById('inicio');
+const question1 = document.getElementById('first');
+comienzo.addEventListener('click',()=>{
+      question1.classList.replace('first','firstA');
+      comienzo.classList.add('ini');
+});
+
+
+
+
+/*next question */
+const  quesion2 = document.getElementById('inicio');
